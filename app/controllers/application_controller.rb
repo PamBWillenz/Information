e927be95e6bf9c484828900328035c5e42800ec5
Pamela Willenz <pam@positek.net>
2017-03-18 18:49:37 +0000

    add first, last names to user for sign-up, in and edit; add validations to user model, test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :sign_up_parms, if: :devise_controller?
+  before_action :account_update_params, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  
+RSpec.describe User, type: :model do 
+  # describe 'creation' do 
+  #   before do 
+  #     current_user = User.create(email: "kitty@meow.com", password: "password", password_confirmation: "password", 
+  #       first_name: "Pixel", last_name: "Kitty")
+  #   end
+
+  #   it 'should be able to be created if valid' do 
+  #     expect(current_user.to be_valid)
+  #   end
+  # end
+
+  # describe 'validations' do 
+  #   it 'should not be valid without a first name' do 
+  #     current_user.first_name = nil
+  #     expect(current_user).to_not be_valid
+  #   end
+
+  #   it 'should not be valid without a last name' do 
+  #     current_user.last_name = nil
+  #     expect(current_user).to_not be_valid
+  #   end
+  # end
 end
