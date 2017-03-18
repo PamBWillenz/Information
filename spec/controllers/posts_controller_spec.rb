d11ff2ea5d8d07e36075ddc4212520523c2d87bb
Pamela Willenz <pam@positek.net>
2017-03-18 15:58:52 +0000

    add simple form, add create action, post params to controller, test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "post#index action" do 
+  describe "posts#index action" do 
     it "should successfully show the page" do 
       get :index
       expect(response).to have_http_status(:success) 
     end
   end
 
-  describe "post#new action" do 
+  describe "posts#new action" do 
     it "should successfully show the new form" do 
       get :new
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "posts#create action" do 
+    it "should successfully create a new post in our database" do 
+      post :create, post: {title: 'Hello!'}
+      expect(response).to redirect_to root_path
+
+      post = Post.last
+      expect(post.title).to eq("Hello!")
     end
   end
 end
